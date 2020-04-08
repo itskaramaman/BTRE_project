cd50a16c09e428d431f39ea3ddf5b4497183f26f
itskaramaman <singhkaramjeetapple@gmail.com>
2020-04-08 17:53:41 +0530

    accounts app created; login register form created

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        # register user
+        return
+    return render(request, 'accounts/register.html')
+
+
+def dashboard(request):
+    if request.method == 'POST':
+        # register user
+        return
+    return render(request, 'accounts/dashboard.html')
+
+
+def logout(request):
+    if request.method == 'POST':
+        # register user
+        return
+    return redirect('index')
